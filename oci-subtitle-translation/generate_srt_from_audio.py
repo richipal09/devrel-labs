ba48efdb3b05027bfae1edc385cf1e25d8f796e9
jasperan <23caj23@gmail.com>
2025-01-14 09:36:37 +0100

    feat: added config loading option for srt generation

--- oci-subtitle-translation/generate_srt_from_audio.py
+++ oci-subtitle-translation/generate_srt_from_audio.py
@@ -13,45 +13,47 @@ config = oci.config.from_file()
 ai_speech_client = oci.ai_speech.AIServiceSpeechClient(config)
 
 
+# Load config from yaml file
+def load_config():
+    """Load configuration from config.yaml"""
+    with open('config.yaml', 'r') as f:
+        return yaml.safe_load(f)
+
+config_yaml = load_config()
+
 # Send the request to service, some parameters are not required, see API
 # doc for more info
 create_transcription_job_response = ai_speech_client.create_transcription_job(
     create_transcription_job_details=oci.ai_speech.models.CreateTranscriptionJobDetails(
-        compartment_id="ocid1.test.oc1..<unique_ID>EXAMPLE-compartmentId-Value",
+        compartment_id=config_yaml['speech']['compartment_id'],
         input_location=oci.ai_speech.models.ObjectListFileInputLocation(
-            location_type="OBJECT_LIST_FILE_INPUT_LOCATION",
+            location_type="OBJECT_LIST_FILE_INPUT_LOCATION", 
             object_location=oci.ai_speech.models.ObjectLocation(
-                namespace_name="EXAMPLE-namespaceName-Value",
-                bucket_name="EXAMPLE-bucketName-Value",
-                object_names=["EXAMPLE--Value"])),
+                namespace_name=config_yaml['speech']['namespace'],
+                bucket_name=config_yaml['speech']['bucket_name'],
+                object_names=["FILE_NAMES"])),
         output_location=oci.ai_speech.models.OutputLocation(
-            namespace_name="EXAMPLE-namespaceName-Value",
-            bucket_name="EXAMPLE-bucketName-Value",
-            prefix="EXAMPLE-prefix-Value"),
-        display_name="EXAMPLE-displayName-Value",
-        description="EXAMPLE-description-Value",
+            namespace_name=config_yaml['speech']['namespace'],
+            bucket_name=config_yaml['speech']['bucket_name'],
+            prefix="transcriptions"),
+        display_name=f"Transcription_{args.input_file}",
+        description=f"Transcription job for {args.input_file}",
         additional_transcription_formats=["SRT"],
         model_details=oci.ai_speech.models.TranscriptionModelDetails(
-            model_type="EXAMPLE-modelType-Value",
             domain="GENERIC",
-            language_code="kn",
+            language_code="en",
             transcription_settings=oci.ai_speech.models.TranscriptionSettings(
                 diarization=oci.ai_speech.models.Diarization(
                     is_diarization_enabled=True,
-                    number_of_speakers=12))),
+                    number_of_speakers=2))),
         normalization=oci.ai_speech.models.TranscriptionNormalization(
             is_punctuation_enabled=True,
             filters=[
                 oci.ai_speech.models.ProfanityTranscriptionFilter(
                     type="PROFANITY",
                     mode="TAG")]),
-        freeform_tags={
-            'EXAMPLE_KEY_reCDt': 'EXAMPLE_VALUE_XNxyFAM9Mof5OQ9ukRcz'},
-        defined_tags={
-            'EXAMPLE_KEY_2qVDu': {
-                'EXAMPLE_KEY_zubEA': 'EXAMPLE--Value'}}),
-    opc_retry_token="EXAMPLE-opcRetryToken-Value",
-    opc_request_id="VL9WEIEFIUXSVY1WP1TJ<unique_ID>")
+        freeform_tags={},
+        defined_tags={}))
 
 # Get the data from response
 print(create_transcription_job_response.data)
